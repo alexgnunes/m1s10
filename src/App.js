5b3b478468de897124333d12581d751cccb03686
alexgnunes <alexgaribalde_tb@hotmail.com>
2024-04-14 16:10:52 -0300

    EX 4 - Crie um Componente de Lista que Receba por Props os dados

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Button from "./components/Button";
+import List from "./components/List";
 import Product from "./components/Product";
 
 function App() {
@@ -6,6 +7,10 @@ function App() {
     <div className="App">
      <Button></Button>
      <Product title='teste' description='hsuhaushuahsuhua' value={55}/>
+     <List nomeAluno= 'Alex'/>
+     <List nomeAluno= 'Elena'/>
+     <List nomeAluno= 'Alex'/>
+     <List nomeAluno= 'Elena'/>
     </div>
   );
 }
---
+++ src/components/List.jsx
@@ -0,0 +1,5 @@
+function List({ nomeAluno }) {
+    return(<ul><li>{nomeAluno}</li></ul>);
+}
+
+export default List;
